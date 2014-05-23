11da4789ede7f0cca7ee58578b5932a1ce5a9cc3
kurata1016 <kurata1016@gmail.com>
2014-05-23 13:27:03 +0900

    second commit

--- OptionMenuSample/src/example/android/optionmenusample/OptionMenuSampleActivity.java
+++ OptionMenuSample/src/example/android/optionmenusample/OptionMenuSampleActivity.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SubMenu;
 
 public class OptionMenuSampleActivity extends Activity {
 
@@ -19,7 +20,29 @@ public class OptionMenuSampleActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_option_menu_sample, menu);
+		// getMenuInflater().inflate(R.menu.activity_option_menu_sample, menu);
+		super.onCreateOptionsMenu(menu);
+
+		MenuItem item1 = menu.add(0, 0, 0, "item1");
+		item1.setIcon(android.R.drawable.ic_menu_add);
+
+		MenuItem item2 = menu.add(0, 1, 0, "item2");
+		item2.setIcon(android.R.drawable.ic_menu_call);
+
+		MenuItem item3 = menu.add(0, 2, 0, "item3");
+		item3.setIcon(android.R.drawable.ic_menu_day);
+
+		MenuItem item4 = menu.add(0, 3, 0, "item4");
+		item4.setIcon(android.R.drawable.ic_menu_my_calendar);
+
+		MenuItem item5 = menu.add(0, 4, 0, "item5");
+		item5.setIcon(android.R.drawable.ic_menu_send);
+
+		SubMenu item6 = menu.addSubMenu(0, 5, 0, "other");
+		item6.setIcon(android.R.drawable.ic_menu_view);
+		item6.add(0, 10, 0, "subitem1");
+		item6.add(0, 20, 0, "subitem2");
+
 		return true;
 	}
 
@@ -28,21 +51,21 @@ public class OptionMenuSampleActivity extends Activity {
 		String action = null;
 		Intent intent = null;
 		switch (item.getItemId()) {
-		case R.id.item1:
-			// showDialog("メニューアイテム1を選択しました。");
+		case 0:
+			 showDialog("メニューアイテム1を選択しました。");
 			return true;
-		case R.id.item2:
+		case 1:
 			showDialog("メニューアイテム2を選択しました。");
 			return true;
-		case R.id.item3:
+		case 2:
 			showDialog("メニューアイテム3を選択しました。");
 			return true;
-		case R.id.subitem1:
+		case 3:
 			action = "android.intent.action.DIAL";
 			intent = new Intent(action);
 			startActivity(intent);
 			return true;
-		case R.id.subitem2:
+		case 4:
 			action = "android.intent.action.VIEW";
 			intent = new Intent(action);
 			intent.setType("vnd.android-dir/mms-sms");
